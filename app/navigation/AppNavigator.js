c5d772f2368221dfe8e949e1228e3780ae0e4308
Mayank Sangwan <98203559+mayanksangwan19@users.noreply.github.com>
2025-07-12 22:05:49 +0530

    Update AppNavigator.js

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnboardingScreen from '../screens/OnboardingScreen';
+import SelectGoalScreen from '../screens/SelectGoalScreen';
+import HomeScreen from '../screens/HomeScreen';
 
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        <Stack.Screen name="SelectGoal" component={SelectGoalScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
