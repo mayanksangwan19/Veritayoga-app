838ac1cc927f802c78fdf1e0ea0decaff8f75875
Mayank Sangwan <98203559+mayanksangwan19@users.noreply.github.com>
2025-07-12 22:16:07 +0530

    Update GoalContext.js

--- context/GoalContext.js
+++ context/GoalContext.js
@@ -1,1 +1,14 @@
+import React, { createContext, useState } from 'react';
+
+export const GoalContext = createContext();
+
+export const GoalProvider = ({ children }) => {
+  const [goal, setGoal] = useState('');
+
+  return (
+    <GoalContext.Provider value={{ goal, setGoal }}>
+      {children}
+    </GoalContext.Provider>
+  );
+};
 
